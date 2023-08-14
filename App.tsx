c8a85172b001b6666dd97a22567c19041abbcf63
Fateh Farooqui <fateh@onloop.io>
2023-08-14 23:44:59 +0530

    Added button

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
+import Button from 'src/Components/Button';
 import Stack from 'src/Components/Stack';
-import Touch from 'src/Components/Touch';
 
 function App() {
   return (
     <Stack flex={1} paddingVertical={'xxl'} paddingHorizontal={'xxl'}>
-      <Touch onPress={() => {}}>
-        <Stack
-          height={50}
-          backgroundColor={'red'}
-          borderRadius={16}
-          elevation={5}
-        />
-      </Touch>
+      <Button>Test</Button>
     </Stack>
   );
 }
